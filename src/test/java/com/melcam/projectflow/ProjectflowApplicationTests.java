58c2e46932ced85152470fe54c102073166a8ec3
melcam <yumelcam.93@gmail.com>
2024-01-23 00:40:21 +0100

    configuration

---
+++ src/test/java/com/melcam/projectflow/ProjectflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melcam.projectflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
